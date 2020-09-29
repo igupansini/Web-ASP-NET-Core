2a273b2d275b543966267568c2731efb1fac03be
Igor Pansini <pansini@outlook.com.br>
2020-09-29 13:35:59 -0300

    Teste MVC Departamento

---
+++ WebApplication/WebApplication/Controllers/DepartamentosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> lista = new List<Departamento>();
+            lista.Add(new Departamento { Id = 1, nome = "Eletronicos" });
+            lista.Add(new Departamento { Id = 2, nome = "Vestuario" });
+            return View(lista);
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Models/Departamento.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string nome { get; set; }
+    }
+}
