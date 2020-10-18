a84333c425f74035b9da75a3cfa857b700ec1302
Igor Pansini <pansini@outlook.com.br>
2020-10-18 16:11:54 -0300

    Deletar Vendedor

--- WebApplication/WebApplication/Services/VendedorService.cs
+++ WebApplication/WebApplication/Services/VendedorService.cs
@@ -26,5 +26,17 @@ namespace WebApplication.Services
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Vendedor FindById(int id)
+        {
+            return _context.Vendedor.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Vendedor.Find(id);
+            _context.Vendedor.Remove(obj);
+            _context.SaveChanges();
+        }
     }
 }
