83982baa90d456e7ec9dae8d320f9e369b78768f
Igor Pansini <pansini@outlook.com.br>
2020-10-23 20:58:23 -0300

    Formatacao geral

--- WebApplication/WebApplication/Models/Vendedor.cs
+++ WebApplication/WebApplication/Models/Vendedor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Rewrite.Internal.IISUrlRewrite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace WebApplication.Models
@@ -9,9 +10,18 @@ namespace WebApplication.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Display(Name = "Salário")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double SalarioBase { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
         public DateTime DataNascimento { get; set; }
+
         public Departamento Departamento { get; set; }
         public int DepartamentoId { get; set; }
         public ICollection<RegistroVenda> Vendas { get; set; } = new List<RegistroVenda>();
